f6075061a7cd3203387ce58c0476c98e67f010ca
cmvn-truongle <truong.le@classmethod.vn>
2023-09-12 21:47:21 +0700

    fix comment change text-align-cnnter to class text-center

--- src/app/pages/auth/login/Login.tsx
+++ src/app/pages/auth/login/Login.tsx
@@ -22,11 +22,13 @@ const Login = () => {
     <div className="auth">
       <div className="auth-wrapper row">
         <div className="auth-body col col-6 col-sm-12">
-          <Link to="/">
-            <img className="logo-img" src={logoImg} alt="image logo Supremethod" />
-          </Link>
-          <h2 className="auth-title">LOGIN</h2>
-          <ul className="login-external-list row">
+          <h1 className='logo'>
+            <Link to="/">
+              <img className="logo-img" src={logoImg} alt="image logo Supremethod" />
+            </Link>
+          </h1>
+          <h2 className="auth-title text-center">LOGIN</h2>
+          <ul className="login-external-list row text-center">
             <li className="login-external-item col col-4">
               <a href="" className="external-item-link">
                 <img src={icGoogle} alt="icon google" className="login-icon icon-google" />
@@ -44,15 +46,15 @@ const Login = () => {
             </li>
           </ul>
           <form action="" className="form login-form">
-            <div className="form-email">
+            <div className="form-input-group">
               <label htmlFor="email" className="form-label">Email</label>
               <input className="input-email form-input" id="email" type="text" placeholder="Enter Email..." />
             </div>
-            <div className="form-password">
+            <div className="form-input-group">
               <label htmlFor="password" className="form-label">Passsword</label>
               <div className="input-password-group">
                 <input className="input-password form-input" id="password" type={isPasswordShow ? "text" : "password"} placeholder="Enter Password..." />
-                <img src={isPasswordShow ? icEyeSlash : icEye} alt="icon eye" className="login-icon icon-eye" onClick={togglePassword} />
+                <img src={isPasswordShow ? icEyeSlash : icEye} alt="icon eye" className="login-icon icon-eye text-center" onClick={togglePassword} />
               </div>
             </div>
             <button type="submit" className="btn btn-primary btn-auth">login</button>
