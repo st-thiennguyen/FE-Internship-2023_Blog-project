589371176a613549fc31eb2004ff76d935d0038f
cmvn-truongle <truong.le@classmethod.vn>
2023-09-12 17:28:11 +0700

    delete !important in scss

--- src/app/pages/auth/login/Login.tsx
+++ src/app/pages/auth/login/Login.tsx
@@ -22,45 +22,45 @@ const Login = () => {
     <div className="auth">
       <div className="auth-wrapper row">
         <div className="auth-body col col-6 col-sm-12">
-          <Link to='/'>
-            <img className='logo-img' src={logoImg} alt="image logo Supremethod" />
+          <Link to="/">
+            <img className="logo-img" src={logoImg} alt="image logo Supremethod" />
           </Link>
           <h2 className="auth-title">LOGIN</h2>
           <ul className="login-external-list row">
             <li className="login-external-item col col-4">
               <a href="" className="external-item-link">
-                <img src={icGoogle} alt="icon google" className='login-icon icon-google' />
+                <img src={icGoogle} alt="icon google" className="login-icon icon-google" />
               </a>
             </li>
             <li className="login-external-item col col-4">
               <a href="" className="external-item-link">
-                <img src={icFacebook} alt="icon facebook" className='login-icon icon-facebook' />
+                <img src={icFacebook} alt="icon facebook" className="login-icon icon-facebook" />
               </a>
             </li>
             <li className="login-external-item col col-4">
               <a href="" className="external-item-link">
-                <img src={icGithub} alt="icon github" className='login-icon icon-github' />
+                <img src={icGithub} alt="icon github" className="login-icon icon-github" />
               </a>
             </li>
           </ul>
           <form action="" className="form login-form">
-            <div className='form-email'>
-              <label htmlFor="email" className='form-label'>Email</label>
-              <input className='input-email form-input' id='email' type="text" placeholder="Enter Email..." />
+            <div className="form-email">
+              <label htmlFor="email" className="form-label">Email</label>
+              <input className="input-email form-input" id="email" type="text" placeholder="Enter Email..." />
             </div>
-            <div className='form-password'>
-              <label htmlFor="password" className='form-label'>Passsword</label>
-              <div className='input-password-group'>
-                <input className='input-password form-input' id='password' type={isPasswordShow ? 'text' : 'password'} placeholder="Enter Password..." />
-                <img src={isPasswordShow ? icEyeSlash : icEye} alt="icon eye" className='login-icon icon-eye' onClick={togglePassword} />
+            <div className="form-password">
+              <label htmlFor="password" className="form-label">Passsword</label>
+              <div className="input-password-group">
+                <input className="input-password form-input" id="password" type={isPasswordShow ? "text" : "password"} placeholder="Enter Password..." />
+                <img src={isPasswordShow ? icEyeSlash : icEye} alt="icon eye" className="login-icon icon-eye" onClick={togglePassword} />
               </div>
             </div>
-            <button type='submit' className='btn btn-primary btn-auth'>login</button>
+            <button type="submit" className="btn btn-primary btn-auth">login</button>
           </form>
-          <p className='text-center'>You're new to Supremethod? <Link to="/register" className='register-link'>Register</Link></p>
+          <p className="text-center">You"re new to Supremethod? <Link to="/register" className="register-link">Register</Link></p>
         </div>
         <div className="auth-bg col col-6">
-          <img src={loginImg} alt="image login" className='auth-img' />
+          <img src={loginImg} alt="image login" className="auth-img" />
         </div>
       </div>
     </div>
@@ -68,4 +68,3 @@ const Login = () => {
 }
 
 export default Login;
-
