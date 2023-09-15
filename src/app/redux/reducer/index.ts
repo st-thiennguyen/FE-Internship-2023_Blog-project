036f2c8422c5d7193e30364b809249d371c6296f
Trong Viet <viet.nguyen@supremetech.vn>
2023-09-15 14:00:34 +0700

    fix conflict

--- src/app/redux/reducer/index.ts
+++ src/app/redux/reducer/index.ts
@@ -1,12 +1,9 @@
 import { combineReducers } from 'redux';
 
 import { detailReducer } from './detail.reducer';
-<<<<<<< HEAD
-import { postReducer } from './post';
-=======
 import loginReducer from './login';
+import { postReducer } from './post';
 import registerReducer from './register';
->>>>>>> 9fb72230d2e1b3254b7fff9b77e089730fe4957e
 
 export const rootReducer = combineReducers({
   login: loginReducer,
