f244e17e830a238947f0bff46ae3012b3868ce02
st-thiennguyen <thien.nguyen@supremetech.vn>
2023-09-17 22:34:37 +0700

    update color of 404 page

--- src/app/pages/not-found/PageNotFound.tsx
+++ src/app/pages/not-found/PageNotFound.tsx
@@ -9,13 +9,14 @@ const PageNotFound = () => {
   return (
     <div className="page-404">
       <div className="container">
-        <div className="page-404-wrapper text-center d-flex justify-between item-center">
+        <div className="page-404-wrapper  d-flex justify-between item-center">
           <div className="page-404-content">
             <h3 className="page-404-status">404</h3>
-            <p className="page-404-title text-center">
+            <p className="page-404-title">
               OOOps! <br /> Page Not Found
             </p>
-            <Button label="Back to Home Page" handleClick={() => navigate('/')} />
+            <p className="page-404-subtitle">This page doesn’t exist or was removed! We suggest you back to home</p>
+            <Button label="Back to Home Page" optionClassName="btn-primary" handleClick={() => navigate('/')} />
           </div>
           <div className="page-404-banner d-flex justify-center item-center">
             <div className="page-404-img">
