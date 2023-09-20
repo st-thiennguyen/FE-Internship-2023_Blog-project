c9eaa8f17b23f89ef89f895f36ebd738f4284f45
st-thiennguyen <thien.nguyen@supremetech.vn>
2023-09-20 14:25:52 +0700

    remove dump import not use

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Layout from './pages/Layout';
 import Login from './pages/auth/containers/Login';
--- src/app/pages/write-post/components/EditorPostTags.tsx
+++ src/app/pages/write-post/components/EditorPostTags.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject, useRef } from 'react';
+import React, { useRef } from 'react';
 
 interface EditorPostTagsProps {
   tags: string[];
