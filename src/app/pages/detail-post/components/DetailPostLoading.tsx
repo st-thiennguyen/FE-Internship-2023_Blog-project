d0d9e91e12e33c9226081b6e8710877e48e780c3
st-thiennguyen <thien.nguyen@supremetech.vn>
2023-09-26 17:30:00 +0700

    fix loading detail page when change div tag to section tag

--- src/app/pages/detail-post/components/DetailPostLoading.tsx
+++ src/app/pages/detail-post/components/DetailPostLoading.tsx
@@ -2,35 +2,37 @@ import { Link } from 'react-router-dom';
 
 const DetailPostLoading = () => {
   return (
-    <div className="detail-page">
-      <section className="section section-detail-cover">
-        <div className="skeleton detail-cover"></div>
-      </section>
-      <section className="section section-detail-content">
-        <div className="detail-content d-flex">
-          <div className="detail-action">
-            <ul className="action-list">
-              {Array.from({ length: 3 }, (item, index) => (
-                <li className="skeleton action-item d-flex item-center" key={index}></li>
-              ))}
-            </ul>
-          </div>
-          <div className="detail-post">
-            <article className="post-content">
-              {Array.from({ length: 30 }, (item, index) => (
-                <p className="skeleton text-row" key={index}></p>
-              ))}
-            </article>
-            <div className="detail-author">
-              <Link to="/" className="detail-author-action text-center d-flex flex-column item-center">
-                <div className="skeleton author-img"></div>
-                <p className="skeleton author-name"></p>
-              </Link>
+    <section className="section-detail-page">
+      <div className="container">
+        <section className="section section-detail-cover">
+          <div className="skeleton detail-cover"></div>
+        </section>
+        <section className="section section-detail-content">
+          <div className="detail-content d-flex">
+            <div className="detail-action">
+              <ul className="action-list">
+                {Array.from({ length: 3 }, (item, index) => (
+                  <li className="skeleton action-item d-flex item-center" key={index}></li>
+                ))}
+              </ul>
+            </div>
+            <div className="detail-post">
+              <article className="post-content">
+                {Array.from({ length: 30 }, (item, index) => (
+                  <p className="skeleton text-row" key={index}></p>
+                ))}
+              </article>
+              <div className="detail-author">
+                <Link to="/" className="detail-author-action text-center d-flex flex-column item-center">
+                  <div className="skeleton author-img"></div>
+                  <p className="skeleton author-name"></p>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </section>
   );
 };
 
