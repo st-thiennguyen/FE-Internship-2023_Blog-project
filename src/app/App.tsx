b83c38821913da1e79cca37ee6f5cbbaa7bd4808
st-khanhngo <khanh.ngo@supremetech.vn>
2023-09-27 15:48:31 +0700

    add scroll to top when change pathname

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 import { RootState } from './stores/store';
 import { RouteItem } from './models/route';
@@ -14,6 +14,12 @@ import { authRoutes } from './pages/auth/auth.routes';
 export const AuthContext = createContext<any>(undefined);
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   const auth = useSelector((state: RootState) => state.auth);
   return (
     <AuthContext.Provider value={auth}>
