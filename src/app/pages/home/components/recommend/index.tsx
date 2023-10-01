3219ec25db1be5b798ad222fa731a335a17f44cb
Trong Viet <viet.nguyen@supremetech.vn>
2023-10-01 22:17:53 +0700

    add loading for recommend

--- src/app/pages/home/components/recommend/index.tsx
+++ src/app/pages/home/components/recommend/index.tsx
@@ -1,30 +1,42 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { RootState } from '../../../../stores/store';
 import PostItem from '../../../../shared/components/PostItem';
 import SectionTitle from '../../../../shared/components/SectionTitle';
+import RecommendItemLoading from './RecommendLoading';
+import { useEffect } from 'react';
+import { getRecommend } from '../../home.actions';
 const Recommend = () => {
   const recommendPosts = useSelector((state: RootState) => state.recommend.data);
-  const isLoading = useSelector((state: RootState) => state.recommend?.isLoading);
+  const isLoading = useSelector((state: RootState) => state.recommend.isLoading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRecommend(1, 10) as any);
+  }, []);
 
   return (
-    <>
-      {
-        isLoading ? <h1 style={{marginTop : "200px"}}>abcskajhjkadsjkfb</h1> : 
-        <section className="section section-recommend">
-          <SectionTitle title="Recommend for you" />
-          <ul className="recommend-list">
-            {recommendPosts.slice(0, 5).map((post) => {
+    <section className="section section-recommend">
+      <SectionTitle title="Recommend for you" />
+      <ul className="recommend-list">
+        {isLoading
+          ? Array.from({ length: 5 }, () => {
+              return (
+                <div className="recommend-item">
+                  <RecommendItemLoading isVertical={true} />
+                </div>
+              );
+            })
+          : recommendPosts.slice(0, 5).map((post) => {
               return (
                 <li className="recommend-item" key={post.id}>
                   <PostItem post={post} isVertical={true} />
                 </li>
               );
             })}
-          </ul>
-        </section>
-      }
-    </>
+      </ul>
+    </section>
   );
 };
 
--- src/app/pages/home/container/Home.tsx
+++ src/app/pages/home/container/Home.tsx
@@ -1,29 +1,12 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-
-import { RootState } from '../../../stores/store';
-import { getRecommend } from '../home.actions';
-
 import GoToTopBtn from '../../../shared/components/GoToTopBtn';
 import LatestPost from '../components/LatestPost';
 import Recommend from '../components/recommend';
-import { getLocalStorage } from '../../../shared/utils';
-import { StorageKey } from '../../../shared/constants';
 
 const Home = () => {
-  const recommendPosts = useSelector((state: RootState) => state.recommend.data);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getRecommend(1, 10) as any);
-  }, []);
-
   return (
     <div className="home-page">
       <div className="container">
-        {recommendPosts.length > 0 && <Recommend />}
+        <Recommend />
         <LatestPost />
         <GoToTopBtn />
       </div>
